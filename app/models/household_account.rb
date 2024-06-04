1e788a18b62ec95ec88a5935b5387685113aad91
Nobuta Taiki <66196738+taikiwish@users.noreply.github.com>
2024-06-04 22:13:39 +0900

    入力時のデフォルト値の削除

--- app/models/household_account.rb
+++ app/models/household_account.rb
@@ -11,24 +11,5 @@ class HouseholdAccount < ApplicationRecord
   validates :year, inclusion: { in: 2024..2040 }
   validates :month_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :year, uniqueness: { scope: :month_id, message: "(and month) has already been taken. Please select a different date." }
-
-  def achievement_rate(budget)
-    (total.to_f / budget.total_budget * 100).round(2)
-  end
-
-  def rank(budget)
-    rate = achievement_rate(budget)
-    case rate
-    when 90..100
-      'S'
-    when 80...90
-      'A'
-    when 70...80
-      'B'
-    when 60...70
-      'C'
-    else
-      'D'
-    end
-  end
+  
 end
